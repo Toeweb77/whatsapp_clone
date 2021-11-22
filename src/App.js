f26a7090826e7bb81f43fdc6e48be79bdddffa7d
Toewebb77 <selelomatlhogonolo@gmail.com>
2021-11-22 12:21:37 +0200

    My files ready

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Whatsapp CLone here</h1>
+
+      <Sidebar/>
+
+    </div>
+
+  );
+}
+
+export default App;
